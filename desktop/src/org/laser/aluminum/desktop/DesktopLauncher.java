01d40af9666d04beee257e459a781035ce6b6aa5
Syfin <masse.wayne@gmail.com>
2017-09-16 07:47:31 -0400

    Initial commit

---
+++ desktop/src/org/laser/aluminum/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.laser.aluminum.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.laser.aluminum.AluminumMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AluminumMain(), config);
+	}
+}
